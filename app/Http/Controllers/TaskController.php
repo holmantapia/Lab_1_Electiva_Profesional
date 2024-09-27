4af09ccede3589afa2174333ed54b5fe6f9974ed
unknown <holmiyt@gmail.com>
2024-09-26 19:17:52 -0500

    Primer commit

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Task;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    public function index()
+    {
+        $tasks = Task::all();
+        return view('tasks.index', compact('tasks'));
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate(['name' => 'required|unique:tasks|max:255']);
+        Task::create($request->only('name'));
+        return redirect()->route('tasks.index');
+    }
+
+    public function destroy(Task $task)
+    {
+        $task->delete();
+        return redirect()->route('tasks.index');
+    }
+}
